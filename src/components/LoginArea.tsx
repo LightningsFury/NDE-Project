4eb5770f87197d378b838a616a714b035e1c72e6
LightningsFury <40724972+LightningsFury@users.noreply.github.com>
2019-09-23 12:41:32 +0100

    submitting gets rid of the text

---
+++ src/components/LoginArea.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export interface LoginAreaProps {
+
+}
+
+export interface LoginAreaState {
+
+}
+
+export class LoginArea extends Component {
+    render() {}
+}

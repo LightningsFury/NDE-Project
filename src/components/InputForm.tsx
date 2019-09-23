752ecf8ffc20405f0d7c1f5df0d6c9ed56a52341
LightningsFury <40724972+LightningsFury@users.noreply.github.com>
2019-09-23 12:03:00 +0100

    input form typing fixing

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -14,14 +14,16 @@ export class InputForm extends Component<InputFormProps, InputFormState> {
     public state: InputFormState = {
         value: ''
     };
-    public submit: React.FormEventHandler<HTMLFormElement> = (e: React.FormEvent<HTMLFormElement>) => {
+    readonly public handleSubmit: React.FormEventHandler<HTMLFormElement> = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
     };
-    public onChange: React.ChangeEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {}
+    readonly public handleChange: React.ChangeEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    }
     render() {
         return (<div>
-            <form onSubmit={this.submit}>
-                <input type={'text'} />
+            <form onSubmit={this.handleSubmit}>
+                <input type={'text'} onChange={this.handleChange} />
                 <input type={'submit'} />
             </form>
         </div>)

6df2bd54bf13a379fe5d5539abee97369973fc77
LightningsFury <40724972+LightningsFury@users.noreply.github.com>
2019-09-30 12:25:20 +0100

    mistyped authenticate

--- src/util/authenticateUser.ts
+++ src/util/authenticateUser.ts
@@ -1,6 +1,6 @@
 import {LoginAreaState} from '../components/LoginArea'
 import { users } from '../data/users.json'
-export const authenticated = (details: LoginAreaState): boolean => {
+export const authenticate = (details: LoginAreaState): boolean => {
     let authenticated = false;
     for (const user of users) {
         if (user.username === details.username && user.password === details.password) authenticated = true;

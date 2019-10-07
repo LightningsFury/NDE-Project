c8d657c740c5009d99ca793d8ca8921d928b5b1a
LightningsFury <40724972+LightningsFury@users.noreply.github.com>
2019-10-07 12:14:25 +0100

    first try at validating error

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import {InputForm} from './components/InputForm';
 import './App.css';
 import {songs} from './data/songs.json';
 import {song} from './interfaces/song'
+import {GameStatus} from './components/GameStatus'
 
 export interface AppState {
   value: string;
   currentSong: song;
+  status: string;
+  secondTry: boolean;
 }
 
 class App extends Component<{}, AppState> {
@@ -20,13 +23,24 @@ class App extends Component<{}, AppState> {
   }
   public state: AppState = {
     value: '',
-    currentSong: this.getRandomSong()
+    currentSong: this.getRandomSong(),
+    secondTry: false,
+    status: ''
   };
   public readonly handleChange: React.ChangeEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({value: e.target.value})
   };
   public readonly handleSubmit: React.FormEventHandler<HTMLFormElement> = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const name = this.state.currentSong.name.toLowerCase();
+    if (this.state.value.toLowerCase() !== name) {
+      this.setState({
+        value: '',
+        secondTry: true, 
+        status: 'Incorrect! Try Again...'
+      })
+      return;
+    }
     this.setState({value: ''})
   }; 
   private generateSongInitials = ({name}: song) => {
@@ -42,6 +56,7 @@ class App extends Component<{}, AppState> {
             <p>{this.state.currentSong.artist}</p>
           </GameArea>
           <InputForm handleSubmit={this.handleSubmit} handleChange={this.handleChange} value={this.state.value} />
+          <GameStatus status={this.state.status} />
       </React.Fragment>
     );
   }

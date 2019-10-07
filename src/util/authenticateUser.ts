5f961ba12332f8ae3e8357cd0d51d5b400e2e499
LightningsFury <40724972+LightningsFury@users.noreply.github.com>
2019-10-07 11:17:06 +0100

    fix encryption logic

--- src/util/authenticateUser.ts
+++ src/util/authenticateUser.ts
@@ -3,8 +3,9 @@ import { users } from '../data/users.json'
 import { encode } from './encryption'
 export const authenticate = (details: LoginAreaState): boolean => {
     let authenticated = false;
+    console.log(encode(details.password))
     for (const user of users) {
-        if (user.username === details.username && encode(user.password) === details.password) authenticated = true;
+        if (user.username === details.username && user.password === encode(details.password)) authenticated = true;
     }    
     return authenticated
 }
